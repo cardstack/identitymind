287e7ca06335f141494407b486deca617e4aa8c5
Alex Speller <alex@alexspeller.com>
2018-05-01 02:37:36 +0100

    Set correct filename for Form A Pdf

--- cardstack/middleware.js
+++ cardstack/middleware.js
@@ -59,6 +59,8 @@ class IdentityMindMiddleware {
       ctxt.status = 200;
       ctxt.body = pdf.toStream();
       ctxt.type = 'pdf';
+      let filename = `FormA-${ctxt.request.query.name}${ctxt.request.query.surname}.pdf`;
+      ctxt.response.set('Content-Disposition', `attachment; filename="${filename}"`);
     });
   }
 
