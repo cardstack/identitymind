b2c3dcc5913552d2fd685840c2ef819fe289b762
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-05-09 20:56:28 -0400

    copy updates

--- addon/validators/file-size.js
+++ addon/validators/file-size.js
@@ -4,15 +4,15 @@ const FileSize = BaseValidator.extend({
   validate(value, options, model, attribute) {
     let fileSize = model.get(`${attribute}FileSize`);
 
-    if (fileSize === undefined) { // user hasn't specified file 
+    if (fileSize === undefined) { // user hasn't specified file
       return options.notPresentMessage || "This file is required";
     }
 
     if (options.maxInMb && fileSize > (options.maxInMb * 1024 * 1024)) {
-      return `Your uploaded file must be smaller than ${options.maxInMb}MB`;
+      return `The file you uploaded exceeds the file size limit of ${options.maxInMb} MB. Please try again.`;
     }
     if (options.minInMb && fileSize < (options.minInMb * 1024 * 1024)) {
-      return `Your uploaded file must be larger than ${options.minInMb}MB`;
+      return `Your uploaded file must be larger than ${options.minInMb} MB`;
     }
     return true;
   }
